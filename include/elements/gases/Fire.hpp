32501516f8347a3b8ebf643d3287178cbdd652c4
PalasanRares <rarespalasan@outlook.com>
2021-12-17 04:16:52 +0200

    added fire burning wood

--- include/elements/gases/Fire.hpp
+++ include/elements/gases/Fire.hpp
@@ -7,6 +7,13 @@ class Fire : public Gas {
 private:
   void disappear(Matrix* matrix, const int& i, const int& j);
 
+  void setWoodOnFire(Matrix* matrix, const int& i, const int& j);
+
+  bool checkTop(Matrix* matrix, const int& i, const int& j);
+  bool checkLeft(Matrix* matrix, const int& i, const int& j);
+  bool checkRight(Matrix* matrix, const int& i, const int& j);
+  bool checkBottom(Matrix* matrix, const int& i, const int& j);
+
 public:
   Fire();
 
--- src/elements/gases/Fire.cpp
+++ src/elements/gases/Fire.cpp
@@ -9,9 +9,50 @@ void Fire::disappear(Matrix* matrix, const int& i, const int& j) {
   }
 }
 
+void Fire::setWoodOnFire(Matrix* matrix, const int& i, const int& j) {
+  if (rand() % 100 < 25) {
+    if (checkTop(matrix, i, j) || checkLeft(matrix, i, j) || checkRight(matrix, i, j) || checkBottom(matrix, i, j)) {
+      return;
+    }
+  }
+}
+
+bool Fire::checkTop(Matrix* matrix, const int& i, const int& j) {
+  if (matrix->checkPosition(i - 1, j) && matrix->getPosition(i - 1, j)->getId() == WOOD) {
+    matrix->setPosition(i - 1, j, new Fire());
+    return true;
+  }
+  return false;
+}
+
+bool Fire::checkLeft(Matrix* matrix, const int& i, const int& j) {
+  if (matrix->checkPosition(i, j - 1) && matrix->getPosition(i, j - 1)->getId() == WOOD) {
+    matrix->setPosition(i, j - 1, new Fire());
+    return true;
+  }
+  return false;
+}
+
+bool Fire::checkRight(Matrix* matrix, const int& i, const int& j) {
+  if (matrix->checkPosition(i, j + 1) && matrix->getPosition(i, j + 1)->getId() == WOOD) {
+    matrix->setPosition(i, j + 1, new Fire());
+    return true;
+  }
+  return false;
+}
+
+bool Fire::checkBottom(Matrix* matrix, const int& i, const int& j) {
+  if (matrix->checkPosition(i + 1, j) && matrix->getPosition(i + 1, j)->getId() == WOOD) {
+    matrix->setPosition(i + 1, j, new Fire());
+    return true;
+  }
+  return false;
+}
+
 Fire::Fire() : Gas(FIRE, GAS, pickFire()) {}
 
 void Fire::update(Matrix* matrix, const int& i, const int& j) {
   Gas::update(matrix, i, j);
+  setWoodOnFire(matrix, i, j);
   disappear(matrix, i, j);
 }
