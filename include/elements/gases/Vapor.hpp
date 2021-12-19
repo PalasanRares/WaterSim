c8231f8a7f8c3870689bae191ad695bfaa962d1d
PalasanRares <rarespalasan@outlook.com>
2021-12-19 08:12:09 +0200

    vapor condensates

--- include/elements/gases/Vapor.hpp
+++ include/elements/gases/Vapor.hpp
@@ -4,8 +4,13 @@
 #include "elements/gases/Gas.hpp"
 
 class Vapor : public Gas {
+private:
+  void condensate(Matrix* matrix, const int& i, const int& j);
+
 public:
   Vapor();
+
+  void update(Matrix* matrix, const int& i, const int& j) override;
 
 };
 
--- src/elements/gases/Vapor.cpp
+++ src/elements/gases/Vapor.cpp
@@ -1,4 +1,17 @@
 #include "elements/gases/Vapor.hpp"
 #include "colorPicker.hpp"
+#include "Matrix.hpp"
+#include "elements/liquids/Water.hpp"
+
+void Vapor::condensate(Matrix* matrix, const int& i, const int& j) {
+  if (rand() % 10000 < 1) {
+    matrix->setPosition(i, j, new Water());
+  }
+}
 
 Vapor::Vapor() : Gas(VAPOR, GAS, pickVapor()) {};
+
+void Vapor::update(Matrix* matrix, const int& i, const int& j) {
+  Gas::update(matrix, i, j);
+  condensate(matrix, i, j);
+}
